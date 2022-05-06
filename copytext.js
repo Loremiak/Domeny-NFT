b26c1ac42717129c4a9e445fa8c602cf85a230ce
Damian Jamróz <mrlorem@gmail.com>
2022-05-06 20:43:44 +0200

    faq update

--- copytext.js
+++ copytext.js
@@ -1,3 +1,5 @@
-function copyMail() {
+const copyMail = () => {
 	navigator.clipboard.writeText("mrlorem@gmail.com");
-}
+	const copyMessageSpan = document.createElement("span");
+	copyMessageSpan.className = "copy-info";
+};
