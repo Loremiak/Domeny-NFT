0bbdf6cbfaa56fed5fc30f03d891a2da3ba16342
Damian Jamróz <mrlorem@gmail.com>
2022-05-28 13:47:57 +0200

    span for mail icon, toggle menu

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+// Toggle menu
+
 const menuBurgerBtn = document.querySelector("#hamburger");
-// const navList = document.querySelector(".nav-list");
+const navList = document.querySelector(".nav-list");
 
 menuBurgerBtn.addEventListener("click", () => {
-	console.log("klik");
+	navList.classList.toggle("nav-list-active");
+});
+
+// Mail copy function
+
+const copyMailBtn = document.querySelector("#copy-btn");
+
+copyMailBtn.addEventListener("click", () => {
+	navigator.clipboard.writeText("mrlorem@gmail.com");
+
+	let copyMessageDOM = document.createElement("span");
+	copyMessageDOM.classList.add("copy-info");
+	copyMessageDOM.innerText = "Mail skopiowany!";
+	copyMailBtn.appendChild(copyMessageDOM);
+
+	const counter = () => {
+		copyMailBtn.removeChild(copyMessageDOM);
+	};
+	setTimeout(counter, 1500);
 });
